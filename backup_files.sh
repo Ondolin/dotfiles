dcb77e769bd464a8463f0f6dcee38de898909392
ondolin <developer@marvin-kaiser.eu>
2021-01-08 16:57:28 +0100

    fixed error in backup_files.sh added update_files.sh to automaticly update files in repo from across the PC

--- backup_files.sh
+++ backup_files.sh
@@ -2,20 +2,21 @@
 
 # create backup folder
 cd ~/dotfiles/removedFiles
-BACKUP_FOLDER = ~/dotfiles/removedFiles/$(date +%Y%m%d_%H%M%S)
-mkdir -p $BACKUP_FOLDER
-echo "Created backup folder at " $BACKUP_FOLDER
+BACKUPFOLDER=~/dotfiles/removedFiles/$(date +%Y%m%d_%H%M%S)
+echo $BACKUPFOLDER
+mkdir -p $BACKUPFOLDER
+echo "Created backup folder at " $BACKUPFOLDER
 
 echo "Start file backup..."
 
 FILE=~/.xinitrc
 if test -f "$FILE"; then
-    cp $FILE $BACKUP_FOLDER
+    cp $FILE $BACKUPFOLDER
 fi
 
 FILE=~/.zshrc
 if test -f "$FILE"; then
-    cp $FILE $BACKUP_FOLDER
+    cp $FILE $BACKUPFOLDER
 fi
 
 echo "Backup complete."
---
+++ update_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -f ~/.xinitrc ~/dotfiles
+cp -f ~/.zshrc ~/dotfiles
