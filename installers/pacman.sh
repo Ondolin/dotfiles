9552a1b30550d1ddc338a5e1bb21f6e1194124a6
ondolin <contact@marvin-kaiser.eu>
2021-07-03 21:59:22 +0000

    added pacman insaller

---
+++ installers/pacman.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )/.."
+
+pacman -Syu
+
+pacman -S --noconfirm curl zsh vim neovim exa
+
+pacman -S --noconfirm python pip
+
+python3 -m pip install pynvim
+
+pacman -S --noconfirm nodejs npm
+
+
+npm install --global neovim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+mkdir -p $HOME/.config
+
+ln -s $DIR/nvim $HOME/.config/nvim
+ln -s $DIR/zsh/.zshrc $HOME
+ln -s $DIR/zsh/.zsh_alias $HOME
+
+nvim +'PlugInstall --sync' +qa
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
