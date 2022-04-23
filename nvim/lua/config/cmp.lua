916adda38d80d6e70f7b9f68284a844ddd2c4880
Ondolin <developer@ondolin.de>
2022-04-23 15:19:19 +0200

    fixed indentation

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -2,12 +2,12 @@ local cmp = require'cmp'
 local lspkind = require('lspkind')
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
 local next_item = function(fallback)
@@ -31,82 +31,75 @@ local previous_item = function()
 end
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-    ['<Esc>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
-     ['<CR>'] = cmp.mapping.confirm({ select = false }),
-    ["<Tab>"] = cmp.mapping(next_item, { "i", "s" }),
-    ["<Down>"] = cmp.mapping(next_item, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(previous_item, { "i", "s" }),
-    ["<Up>"] = cmp.mapping(previous_item, { "i", "s" }),
-  },
-  formatting = {
-    format = lspkind.cmp_format({
-      mode = 'symbol_text',
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = {
+        ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+        ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+        ['<C-e>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<Esc>'] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+        }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping(next_item, { "i", "s" }),
+        ["<Down>"] = cmp.mapping(next_item, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(previous_item, { "i", "s" }),
+        ["<Up>"] = cmp.mapping(previous_item, { "i", "s" }),
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-        return vim_item
-      end,
-      menu = ({
-          buffer = "[BUF]",
-          omni = "[OMNI]",
-          nvim_lsp = "[LSP]",
-          path = "[PATH]",
-          vsnip = "[SNIP]",
-          cmp_git = "[GIT]"
-      })
+            -- The function below will be called before any actual modifications from lspkind
+            -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+            before = function (entry, vim_item)
+                return vim_item
+            end,
+            menu = ({
+                buffer = "[BUF]",
+                omni = "[OMNI]",
+                nvim_lsp = "[LSP]",
+                path = "[PATH]",
+                vsnip = "[SNIP]",
+                cmp_git = "[GIT]"
+            })
+        })
+    },
+    sources = cmp.config.sources({
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        -- { name = 'omni' },
+        { name = 'vsnip' },
+        { name = 'nvim_lua' },
+        { name = 'rg' }
+    }, {
+        { name = 'buffer' },
     })
-  },
-  -- completion = {
-  --   keyword_length = 1,
-  --   completeopt = "menu,noselect"
-  -- },
-  -- view = {
-  --   entries = 'custom',
-  -- },
-  sources = cmp.config.sources({
-    { name = 'path' },
-    { name = 'nvim_lsp' },
-    -- { name = 'omni' },
-    { name = 'vsnip' },
-    { name = 'nvim_lua' },
-    { name = 'rg' }
-  }, {
-    { name = 'buffer' },
-  })
 })
 
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' },
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        { name = 'cmp_git' },
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 require("cmp_git").setup()
 
 cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
-  }
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 -- cmp.setup.cmdline(':', {
